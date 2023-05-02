abb12192cf81c2a9f2990d21eda2c7fa0099bd7e
Maaz Ahmad Khan <maaz95734@gmail.com>
2023-05-03 00:00:35 +0530

    bug fix

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,10 +1,5 @@
-import {
-  GetStaticPaths,
-  GetStaticPathsResult,
-  GetStaticProps,
-  GetStaticPropsContext,
-  GetStaticPropsResult,
-} from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
@@ -34,12 +29,14 @@ export const getStaticProps: GetStaticProps<
 };
 
 export const getStaticPaths: GetStaticPaths<CoffeeStoreParams> = () => {
+  const paths = coffessStores.map((coffeStore) => {
+    return {
+      params: { id: String(coffeStore.id) },
+    };
+  });
   return {
-    paths: [
-      { params: { id: "0" } },
-      { params: { id: "1" } },
-      { params: { id: "300" } },
-    ],
+    paths,
+    //fall back true will render for unspecified paths while fallback false returns 404 page
     fallback: false,
   };
 };
@@ -49,11 +46,21 @@ type CoffeeStoreProps = CoffeeStoreStaticProps & {};
 const CoffeeStore = (props: CoffeeStoreProps): JSX.Element => {
   const router = useRouter();
   console.log(props.coffeeStore?.name);
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  // destructure props after router.isFallback check
+  const { coffeeStore } = props;
+  const { address, id, imgUrl, name, neighbourhood, websiteUrl } =
+    coffeeStore || {};
   return (
-    <>
+    <div>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <Link href={"/"}>Back to home</Link>
       <div>Coffee Store {router.query.id}</div>
-      <Link href={`/`}>Back to home</Link>
-    </>
+    </div>
   );
 };
 
